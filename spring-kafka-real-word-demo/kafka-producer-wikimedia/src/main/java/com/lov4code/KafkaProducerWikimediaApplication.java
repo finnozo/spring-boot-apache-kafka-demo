faa0f9fb4b1d10f5eac4a6e6d47d31c5ffbb3426
Sushil Dangi <dangi.sushil5@gmail.com>
2022-12-10 23:46:37 +0530

    multi module project setup done

---
+++ spring-kafka-real-word-demo/kafka-producer-wikimedia/src/main/java/com/lov4code/KafkaProducerWikimediaApplication.java
@@ -0,0 +1,13 @@
+package com.lov4code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProducerWikimediaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaProducerWikimediaApplication.class, args);
+    }
+
+}
